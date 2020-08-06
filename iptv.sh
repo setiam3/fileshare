91245080fbcf14c6906dd69270f3191e14de4696
setiam3 <setiam3@gmail.com>
2020-08-07 00:58:37 +0700

    updated..

--- iptv.sh
+++ iptv.sh
@@ -25,11 +25,11 @@ IPTV9="$iptvdir/iptv/m3u/vidio.com/mnctv/mnc.php"
 IPTV10="$iptvdir/iptv/m3u/vidio.com/molatv/molatvfree.php"
 IPTV11="$iptvdir/iptv/m3u/vidio.com/mytv/mytv.php"
 IPTV12="$iptvdir/iptv/m3u/vidio.com/NET.HD/net.php"
-IPTV13="$iptvdir/iptv/m3u/vidio.com/ochanel/ochanel.php"
-IPTV14="$iptvdir/iptv/m3u/vidio.com/ochanel/ochanel.php"
+IPTV13="$iptvdir/iptv/m3u/vidio.com/ochannel/ochannel.php"
+IPTV14="$iptvdir/iptv/m3u/vidio.com/primer/CitraBioskop/bioskop.php"
 IPTV15="$iptvdir/iptv/m3u/vidio.com/oshop/oshop.php"
 IPTV16="$iptvdir/iptv/m3u/vidio.com/primer/CitraDrama/citradrama.php"
-IPTV17="$iptvdir/iptv/m3u/vidio.com/primer/CitraEntertainment/cent.php"
+IPTV17="$iptvdir/iptv/m3u/vidio.com/primer/CitraEntertaiment/cent.php"
 IPTV18="$iptvdir/iptv/m3u/vidio.com/rcti/rcti.php"
 IPTV19="$iptvdir/iptv/m3u/vidio.com/rtv/rtv.php"
 IPTV20="$iptvdir/iptv/m3u/vidio.com/sctv/sctv.php"
@@ -43,19 +43,15 @@ IPTVS=("$IPTV1" "$IPTV2" "$IPTV3" "$IPTV4" "$IPTV5" "$IPTV6" "$IPTV7" "$IPTV8" "
 jumlahtv=${#IPTVS[@]}
 sleeptime="1"
 
-num=1
-    for i in "${#IPTVS[@]}"
-    do
-    target="IPTV$num"
-    #    	/usr/bin/php "IPTV$num"
-		$phppath ${!target}
-    echo $num
-    echo " "
-    sleep $sleeptime
-    let num++;
-    done
-
-sleep 20
+counter=1
+while [ $counter -le $jumlahtv ]
+do
+  target="IPTV$counter"
+  /usr/bin/php ${!target}
+  sleep $sleeptime
+((counter++))
+done
+sleep 2
 git add .
 sleep $sleeptime
 git commit -m "updated.."
